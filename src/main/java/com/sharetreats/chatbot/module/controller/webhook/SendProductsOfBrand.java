9aaedaee0761d178ae020cb18410ba62df3ba8ff
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-06-27 14:12:46 +0900

    feat:  각 기능에 맞게 수행되는 분기 클래스 구현 (#19)
    해당 구현은 예시입니다. 본인 구현 로직에 맞게 수정해주세요. 
    
    최대한 충돌을 방지하고자 각 기능마다 동작하는 클래스로 분리했습니다.

---
+++ src/main/java/com/sharetreats/chatbot/module/controller/webhook/SendProductsOfBrand.java
@@ -0,0 +1,10 @@
+package com.sharetreats.chatbot.module.controller.webhook;
+
+import org.springframework.http.ResponseEntity;
+
+public class SendProductsOfBrand {
+    public static ResponseEntity<?> execute(String callback) {
+
+        return null;
+    }
+}
