ab7550e6c20efb0cd9f5d36f31f27828d7344257
tlqkrus012345 <tlqkrus012345@naver.com>
2023-06-28 12:24:52 +0900

    feat: webhook연결을 위한 메서드 추가 (#10)

--- src/main/java/com/sharetreats/chatbot/module/controller/HealthCheckController.java
+++ src/main/java/com/sharetreats/chatbot/module/controller/HealthCheckController.java
@@ -1,11 +1,20 @@
 package com.sharetreats.chatbot.module.controller;
 
+import com.sharetreats.chatbot.infra.config.WebhookConfig;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequiredArgsConstructor
 @RestController
 public class HealthCheckController {
 
+    private final WebhookConfig webhookConfig;
+    private boolean webhookSet = false;
     @GetMapping("/health")
-    public void healthCheck() {}
+    public void healthCheck() {
+        if (!webhookSet) {
+            webhookConfig.setWebhook();
+            webhookSet = true;
+        }
+    }
 }
