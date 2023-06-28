c9e2aa183be34be1eeee58c7a6d0709e522ba39e
Damm06 <ghkeka111@gmail.com>
2023-06-28 22:22:12 +0900

    feat : welcome 컴포넌트 클래스에 실제 welcome 메세지 전송 기능 동작 코드 병합 (#6)

--- src/main/java/com/sharetreats/chatbot/module/controller/webhook/SendWelcomeMessage.java
+++ src/main/java/com/sharetreats/chatbot/module/controller/webhook/SendWelcomeMessage.java
@@ -1,26 +1,44 @@
 package com.sharetreats.chatbot.module.controller.webhook;
 
-import com.sharetreats.chatbot.module.controller.dto.Dto;
-import com.sharetreats.chatbot.module.service.ServiceTest;
+import com.sharetreats.chatbot.module.controller.dto.WelcomeButton;
+import com.sharetreats.chatbot.module.controller.dto.WelcomeKeyboard;
+import com.sharetreats.chatbot.module.controller.dto.WelcomeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
- * 예시를 위한 클래스입니다.
+ * welcome message 전송 기능을 위한 클래스 입니다.
  */
 @RequiredArgsConstructor
 @Component
 public class SendWelcomeMessage extends ResponseEvent {
 
-    private final ServiceTest service;
+    @Value("${viber.auth.token}")
+    private String token;
+
+    @Value("${viber.avatar}")
+    private String avatar;
+
+    @Value("${viber.welcome-media}")
+    private String welcome_media;
 
     @Override
-    public ResponseEntity<?> execute(String callback) {
-        service.test();
+    public ResponseEntity<WelcomeMessage> execute(String callback) {
         return ResponseEntity.ok(
-                Dto.builder()
-                        .text("예시를 위한 dto 입니다. 확인 후 삭제해주세요.")
-                        .build());
+                WelcomeMessage.of("Viber Treats", avatar,"tracking_data", "picture",
+                        "\uD83E\uDD73\uD83C\uDF89 Hello. Welcome to Viber Treats! We have various treats to send to your friends.\n" +
+                                "Click " + "\"Show Treats\"" + " to start",
+                        WelcomeKeyboard.of("keyboard", false,"#FFFFFF", createButtons()),
+                        welcome_media, welcome_media));
     }
+
+    public List<WelcomeButton> createButtons() {
+        return Collections.singletonList(WelcomeButton.of());
+    }
+
 }
