4d675af88dcc7d6870325817dc95056db6363cd9
Damm06 <ghkeka111@gmail.com>
2023-06-30 13:37:08 +0900

    fix : S3Config의 S3 환경변수 경로 수정 (#21)

--- src/main/java/com/sharetreats/chatbot/infra/config/S3Config.java
+++ src/main/java/com/sharetreats/chatbot/infra/config/S3Config.java
@@ -11,13 +11,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-    @Value("${cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.s3.credentials.access-key}")
     private String accessKey;
 
-    @Value("${cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.s3.credentials.secret-key}")
     private String secretKey;
 
-    @Value("${cloud.aws.region.static}")
+    @Value("${cloud.aws.s3.region.static}")
     private String region;
 
     @Bean
