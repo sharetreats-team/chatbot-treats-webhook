1b6957a9ba5b1d2a45525a46efbe904f2190dc29
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-07-03 11:14:15 +0900

    bug: email service bean 충돌 오류 해결

--- src/main/java/com/sharetreats/chatbot/infra/mail/ConsoleEmailService.java
+++ src/main/java/com/sharetreats/chatbot/infra/mail/ConsoleEmailService.java
@@ -1,11 +1,12 @@
 package com.sharetreats.chatbot.infra.mail;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Profile("local")
+@Primary
 @Component
 public class ConsoleEmailService implements EmailService {
 
