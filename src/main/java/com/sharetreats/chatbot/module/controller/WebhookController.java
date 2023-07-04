86ed257cae3b7c4b4720c104e48a82bdd6e5f5e1
Kim Dael <daelkdev@gmail.com>
2023-07-04 11:35:04 +0900

    "[update] 선물 구매 정보 입력 - 할인코드 입력하지 않는 선택지 추가 "

--- src/main/java/com/sharetreats/chatbot/module/controller/WebhookController.java
+++ src/main/java/com/sharetreats/chatbot/module/controller/WebhookController.java
@@ -60,7 +60,7 @@ public class WebhookController {
             return sendPaymentResultMessage.execute(callback);
         if (isContains(text, VIEW_PRODUCTS_OF_BRAND))
             return sendProductsOfBrand.execute(callback);
-        if (isContains(text, SEND_TREATS) || isTrackingDataValid(trackingData))
+        if (isContains(text, SEND_TREATS) || isContains(text, NO_DISCOUNT) || isTrackingDataValid(trackingData))
             return sendPurchaseInfo.execute(callback);
         if (isContains(text, VIEW_MORE))
             return sendproductDetail.execute(callback);
@@ -103,5 +103,6 @@ public class WebhookController {
         public static final String VIEW_PRODUCTS_OF_BRAND = "brandId";
         public static final String SEND_TREATS = "send treats";
         public static final String VIEW_MORE = "view more";
+        public static final String NO_DISCOUNT = "no discount";
     }
 }
