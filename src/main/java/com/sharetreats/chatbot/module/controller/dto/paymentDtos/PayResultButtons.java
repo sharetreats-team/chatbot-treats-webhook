ee8deab5461571e36f553c3cb55910713df27343
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-07-04 12:26:32 +0900

    test: rich-media 동작 테스트 (3)

--- src/main/java/com/sharetreats/chatbot/module/controller/dto/paymentDtos/PayResultButtons.java
+++ src/main/java/com/sharetreats/chatbot/module/controller/dto/paymentDtos/PayResultButtons.java
@@ -16,8 +16,8 @@ public class PayResultButtons {
     public static List<PayResultButtons> of(GiftHistory giftHistory) {
         return List.of(
                 ButtonsForImage.of(6, 6, "none", "https://www.google.com", getProductImage(giftHistory)),
-                ButtonsForText.of(6, 1, "none", "상품제목", "<font color=#323232><b>" + getProductName(giftHistory) + "</b></font>", "large", "middle", "left"));
-                //ButtonsForText.of(6, 1, "none", "브랜드명", "<font color=#323232>" + getBrandNameOfProduct(giftHistory) + "</font>", "small", "top", "left"),
+                //ButtonsForText.of(6, 1, "none", "상품제목", "<font color=#323232><b>" + getProductName(giftHistory) + "</b></font>", "large", "middle", "left"),
+                ButtonsForText.of(6, 1, "none", "브랜드명", "<font color=#323232>" + getBrandNameOfProduct(giftHistory) + "</font>", "small", "top", "left"));
 //                ButtonsForText.of(6, 4, "none", "발신자명,메시지",
 //                        "<font color=#323232>From. " + getSenderName(giftHistory) + "</font><br>"
 //                                + "<font color=#323232>Message:" + getMessage(giftHistory) + "</font><br>"
