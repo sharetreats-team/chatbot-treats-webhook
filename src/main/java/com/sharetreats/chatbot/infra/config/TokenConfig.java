56aba88f5f0b0bc4c5eb521d81d52d379db6c5b9
tlqkrus012345 <tlqkrus012345@naver.com>
2023-07-06 12:16:35 +0900

    update: 토큰 저장소 Redis로 변경 (#43)

--- src/main/java/com/sharetreats/chatbot/infra/config/TokenConfig.java
+++ src/main/java/com/sharetreats/chatbot/infra/config/TokenConfig.java
@@ -1,38 +1,41 @@
 package com.sharetreats.chatbot.infra.config;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class TokenConfig {
-    private static final long TOKEN_EXPIRE_TIME = 30 * 60 * 1000;
-    private static final ConcurrentHashMap<String, TokenInfo> tokenStore = new ConcurrentHashMap<>();
-    public void generateToken(String userId) {
-        String token = UUID.randomUUID().toString();
-        long expireTime = System.currentTimeMillis() + TOKEN_EXPIRE_TIME;
-        TokenInfo tokenInfo = new TokenInfo(token, expireTime);
-        tokenStore.put(userId, tokenInfo);
-    }
 
+    private final RedisTemplate<String, String> redisTemplate;
+    public void generateToken(String userId) {
+
+        String token = UUID.randomUUID().toString();
+        Duration expireDuration = Duration.ofMinutes(20);
+        redisTemplate.opsForValue().set(userId, token, expireDuration);
+
+    }
     public boolean validateToken(String userId) {
-        TokenInfo tokenInfo = tokenStore.get(userId);
-        if (tokenInfo != null && tokenInfo.getExpireTime() >= System.currentTimeMillis()) {
-            return true;
-        } else {
-            tokenStore.remove(userId);
+
+        try {
+            String token = redisTemplate.opsForValue().get(userId);
+            if (token != null) {
+                Long expireTime = redisTemplate.getExpire(userId);
+                if (expireTime != null && expireTime > 0) {
+                    return true;
+                }
+            }
+        } catch (IllegalArgumentException e) {
             return false;
         }
-    }
-    @Getter
-    private class TokenInfo {
-        private String token;
-        private long expireTime;
-        public TokenInfo(String token, long expireTime) {
-            this.token = token;
-            this.expireTime = expireTime;
-        }
+        return false;
     }
 }
