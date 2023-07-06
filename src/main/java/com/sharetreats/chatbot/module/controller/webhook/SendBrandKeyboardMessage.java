77a487f75065746f7d00e1b81256f37f78a609be
Damm06 <ghkeka111@gmail.com>
2023-07-06 15:36:11 +0900

    update : 브랜드 목록 조회 시 categoryId param 추가 (#84)

--- src/main/java/com/sharetreats/chatbot/module/controller/webhook/SendBrandKeyboardMessage.java
+++ src/main/java/com/sharetreats/chatbot/module/controller/webhook/SendBrandKeyboardMessage.java
@@ -15,15 +15,19 @@ import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class SendBrandKeyboardMessage extends ResponseEvent {
+public class SendBrandKeyboardMessage {
     private final BrandService brandService;
     private final RestTemplateConfig restTemplateConfig;
     static final String VIBER_SEND_MESSAGE_URL = "https://chatapi.viber.com/pa/send_message";
     @Value("${viber.auth.token}")
     private String token;
 
-    public ResponseEntity<?> execute(String callback) {
-        BrandResponse response = findBrandListForReceiverId(getIdFromSender(callback));
+    public ResponseEntity<?> execute(String callback, String text) {
+
+        String strCategoryId = text.substring(11);
+        Long categoryId = Long.parseLong(strCategoryId);
+
+        BrandResponse response = findBrandListForReceiverId(getIdFromSender(callback), categoryId);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -34,12 +38,13 @@ public class SendBrandKeyboardMessage extends ResponseEvent {
         return restResponse;
     }
 
-    public BrandResponse findBrandListForReceiverId(String id) {
-        BrandKeyboard keyboard = BrandKeyboard.of(brandService.createBrandButtons());
+    public BrandResponse findBrandListForReceiverId(String id, Long categoryId) {
+        BrandKeyboard keyboard = BrandKeyboard.of(brandService.createBrandButtons(categoryId));
         return BrandResponse.of(id, keyboard);
     }
 
     private static String getIdFromSender(String callback) {
         return new JSONObject(callback).getJSONObject("sender").getString("id");
     }
+
 }
