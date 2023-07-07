daf9dbe76037a086c6bcb1b4bd9f3781f15e8d93
tlqkrus012345 <tlqkrus012345@naver.com>
2023-07-07 12:34:28 +0900

    update: ElastiCache에서 서버에 직접 redis 클러스터 구성으로 변경 (#43)

--- src/main/java/com/sharetreats/chatbot/infra/config/RedisConfig.java
+++ src/main/java/com/sharetreats/chatbot/infra/config/RedisConfig.java
@@ -1,11 +1,14 @@
 package com.sharetreats.chatbot.infra.config;
 
+import io.lettuce.core.ReadFrom;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisNode;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -14,36 +17,28 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
-    @Value("${spring.redis.gift-history.host}")
-    private String redisHost;
-
-    @Value("${spring.redis.gift-history.port}")
-    private int redisPort;
-
-    @Value("${spring.redis.token.host}")
-    private String tokenRedisHost;
-
-    @Value("${spring.redis.token.port}")
-    private int tokenRedisPort;
-
     @Bean
-    @Primary
-    public LettuceConnectionFactory connectionFactory() {
-        return new LettuceConnectionFactory(redisHost, redisPort);
-    }
+    public RedisConnectionFactory redisConnectionFactory() {
+        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+                .readFrom(ReadFrom.REPLICA_PREFERRED)
+                .build();
 
-    @Bean
-    public LettuceConnectionFactory tokenConnectionFactory() {
-        RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration();
-        clusterConfig.addClusterNode(new RedisNode(tokenRedisHost, tokenRedisPort));
+        RedisClusterConfiguration redisClusterConfiguration = new RedisClusterConfiguration()
+                .clusterNode("localhost", 8000)
+                .clusterNode("localhost", 8001)
+                .clusterNode("localhost", 8002)
+                .clusterNode("localhost", 8003)
+                .clusterNode("localhost", 8004)
+                .clusterNode("localhost", 8005);
 
-        return new LettuceConnectionFactory(clusterConfig);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisClusterConfiguration, clientConfiguration);
+        return lettuceConnectionFactory;
     }
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setConnectionFactory(connectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
 
@@ -52,7 +47,7 @@ public class RedisConfig {
     public RedisTemplate<String, String> tokenRedisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setDefaultSerializer(new StringRedisSerializer());
-        redisTemplate.setConnectionFactory(tokenConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
 }
