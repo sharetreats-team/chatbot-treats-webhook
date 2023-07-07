968eccf7b34a2c966cd9676d2a5bb793d96dac4e
Si-Hyeak-KANG <zlcls456@gmail.com>
2023-07-07 15:26:17 +0900

    feat: 각 Entity Init data SQL 작성 (#86)

--- src/main/resources/db/discount-code-data.sql
+++ src/main/resources/db/discount-code-data.sql
@@ -1,5 +1,5 @@
 INSERT INTO discount_code (code, type, discount_price)
 VALUES ('CODE1', 'RATE_DISCOUNT', 10),
-('CODE2', 'FIX_DISCOUNT', 10),
+('CODE2', 'FIX_DISCOUNT', 100),
 ('CODE3', 'RATE_DISCOUNT', 20),
-('CODE4', 'FIX_DISCOUNT', 20);
+('CODE4', 'FIX_DISCOUNT', 200);
